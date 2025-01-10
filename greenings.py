2a725d0efdb26239439c6b6b9b37000b0f56f4ea
SerjKosov <serj200000@mail.ru>
2025-01-10 21:48:45 +0700

    Initial commit

---
+++ greenings.py
@@ -0,0 +1,12 @@
+username=' Сергей '
+title='заголовок'
+content='описание'
+status='статус'
+created_date='10.01.25'
+issue_date='12.01.25'
+print("имя пользователя:", username)
+print("заголовок заметки:", title)
+print("описание заметки:",content)
+print("статус заметки:",status)
+print("дата создания:",created_date)
+print("дата окончания:",issue_date)
