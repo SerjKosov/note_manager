437c2684bc9fdc2856d19ad704482a630b4c73fd
SerjKosov <serj200000@mail.ru>
2025-01-10 22:28:45 +0700

    Update greenings.py

--- greenings.py
+++ greenings.py
@@ -1,9 +1,9 @@
-username=' Сергей '
-title='заголовок'
-content='описание'
-status='статус'
-created_date='10.01.25'
-issue_date='12.01.25'
+username =' Сергей '
+title ='заголовок'
+content ='описание'
+status ='статус'
+created_date ='10.01.25'
+issue_date ='12.01.25'
 print("имя пользователя:", username)
 print("заголовок заметки:", title)
 print("описание заметки:",content)
