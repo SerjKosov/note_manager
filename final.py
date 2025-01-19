d6a16755796c38b1163af22257c13aa64e659ad9
SerjKosov <serj200000@mail.ru>
2025-01-19 17:18:22 +0700

    Add files via upload

---
+++ final.py
@@ -0,0 +1,8 @@
+name = input("Введите имя пользователя: ")
+content = input("Содержание заметки: ")
+status = input("определите статус заметки: ")
+created_date = input("Введите дату создания: ")
+issue_date = input("Введите дату изменения: ")
+title1 = input("Введите заголовок 1: ")
+title2 = input("Введите заголовок 2: ")
+note = [ name, content, status, created_date, issue_date, [title1,  title2]]
