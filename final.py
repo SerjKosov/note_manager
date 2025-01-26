7a733d34ad76c14a37a269797ea8e90dc29f9aa0
SerjKosov <serj200000@mail.ru>
2025-01-27 05:29:27 +0700

    Update final.py

--- final.py
+++ final.py
@@ -1,9 +1,10 @@
 name = input("Введите имя пользователя: ")
-content = input("Содержание заметки: ")
+content = input("Название заметки: ")
 status = input("определите статус заметки: ")
 created_date = input("Введите дату создания: ")
 issue_date = input("Введите дату изменения: ")
 title1 = input("Введите заголовок 1: ")
 title2 = input("Введите заголовок 2: ")
 note = [ name, content, status, created_date, issue_date, [title1,  title2]]
-print(note)
+print(f"Имя пользователя: {name}, \nдата создания: {created_date[0:5]}, \nдата завершения: {issue_date[0:5]},"
+      f"\nстатус заметки: {status}, \nНазвание заметки: {content}, \n{title1}, {title2}")
